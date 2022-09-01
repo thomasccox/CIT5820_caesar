f232631a9768c7b92cb0a0e3ca73e1258aef07a0
thomascox <coxt@seas.upenn.edu>
2022-09-01 18:23:48 -0400

    commit4 return to base

--- caesar.py
+++ caesar.py
@@ -6,33 +6,33 @@ def encrypt(key, plaintext):
     length = len(plaintext)
     cap = []
     low = []
-    for x in range(ord('A'), ord('Z')+1):
-       cap.append(x)
-       #print(x)
-    for y in range(ord('a'), ord('z')+1):
+    for x in range(ord('A'), ord('Z') + 1):
+        cap.append(x)
+
+    for y in range(ord('a'), ord('z') + 1):
         low.append(y)
-        #print(y)
 
     for x in range(length):
         orig = ord(plaintext[x])
         crypt = orig + key
-        #print(orig)
+
         if key >= 0:
             if (orig in cap) and crypt > ord('Z'):
                 crypt = ord('A') + (crypt - (ord('Z') + 1))
-                #print(chr(crypt))
+
             elif (orig in low) and crypt > ord('z'):
                 crypt = ord('a') + (crypt - (ord('z') + 1))
+
         else:
             if (orig in cap) and crypt < ord('A'):
                 crypt = ord('Z') - (ord('A') - (crypt + 1))
                 print(chr(crypt))
+
             elif (orig in low) and crypt > ord('z'):
                 crypt = ord('z') - (ord('z') - (crypt + 1))
 
         ciphertext = ciphertext + chr(crypt)
-        #print(chr(crypt))
-        #print(ciphertext)
+
     return ciphertext
 
 
@@ -41,34 +41,29 @@ def decrypt(key, ciphertext):
     length = len(ciphertext)
     cap = []
     low = []
-    for x in range(ord('A'), ord('Z')+1):
-       cap.append(x)
-       #print(x)
-    for y in range(ord('a'), ord('z')+1):
+    for x in range(ord('A'), ord('Z') + 1):
+        cap.append(x)
+
+    for y in range(ord('a'), ord('z') + 1):
         low.append(y)
-        #print(y)
 
     for x in range(length):
         crypt = ord(ciphertext[x])
         orig = crypt - key
-        #print(chr(crypt))
-        #print(chr(orig))
+
         if key >= 0:
             if (crypt in cap) and orig < ord('A'):
                 orig = ord('Z') - (ord('A') - (orig + 1))
-                #print(chr(crypt))
-                #print(chr(orig))
+
             elif (crypt in low) and orig < ord('a'):
                 orig = ord('z') - (ord('a') - (orig + 1))
         else:
             if (crypt in cap) and orig > ord('Z'):
-                orig = ord('A') + (orig - (ord('Z')+1))
-                #print(chr(crypt))
-                #print(chr(orig))
+                orig = ord('A') + (orig - (ord('Z') + 1))
+
             elif (crypt in low) and orig < ord('a'):
-                orig = ord('a') + (orig - (ord('z')+1))
+                orig = ord('a') + (orig - (ord('z') + 1))
 
         plaintext = plaintext + chr(orig)
-        #print(chr(orig))
-        #print(plaintext)
+
     return plaintext
